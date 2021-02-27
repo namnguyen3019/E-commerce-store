7726a451a0f8e596d543711ac6cb40c2fe6092f3
Nam Nguyen <namnguyn3019@gmail.com>
2021-02-27 17:56:26 -0600

    fixed bug

--- backend/server.js
+++ backend/server.js
@@ -37,15 +37,17 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/build')))
+
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	)
 } else {
 	app.get('/', (req, res) => {
-		res.send('API is running')
+		res.send('API is running....')
 	})
 }
 const PORT = process.env.PORT || 5000
+
 app.listen(
 	PORT,
 	console.log(`server running in ${process.env.NODE_ENV} on port, ${PORT}`)
